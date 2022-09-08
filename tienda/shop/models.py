5d5db16d22e26967f3d66ab7fd1d9a209eb66b72
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-09-08 16:14:02 -0300

    New Fields in Category

--- tienda/shop/models.py
+++ tienda/shop/models.py
@@ -15,6 +15,7 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     is_active = models.BooleanField()
+    category_id = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
     
